f647485e8f285e800e55cc5abdd6d49141c2fd03
Andre <andre@A-MacBook-Air.local>
2022-02-08 22:29:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = process.env.REACT_APP_API_KEY;
+const HASH = process.env.REACT_APP_API_HASH;
+const TS = process.env.REACT_APP_API_TS;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const LIMIT = 99;
+
+// const baseUrl = "http://gateway.marvel.com/v1/public/characters"
+// const query = `?limit=${req.query.limit}&nameStartsWith=${req.query.name}`;
+//hash - a md5 digest of the ts parameter, your private key and your public key (e.g. md5(ts+privateKey+publicKey)
+
+class App extends Component {
+  state = {
+    marvel: "",
+  };
+
+  componentDidMount() {
+    axios
+      .get(`${BASE_URL}?limit=${LIMIT}&apikey=${API_KEY}&hash=${HASH}&ts=${TS}`)
+      .then((res) => {
+        let display = res.data.data.results.map((e, i) => {
+          return <li key={i}>{e.name}</li>;
+        });
+        console.log(display);
+
+        this.setState({
+          marvel: display,
+        });
+      })
+      .catch((e) => console.log("what is the error", e));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My favorite Marvel is:</h1>
+        {this.state.marvel}
+      </div>
+    );
+  }
 }
 
 export default App;
